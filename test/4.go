3d09518a1a7c3505695789874e9abf6b0a5f1791
Yash Srivastav <admin@yashsriv.org>
2018-01-25 17:52:11 +0530

    Add test files

---
+++ test/4.go
@@ -0,0 +1,31 @@
+package main
+
+import "fmt"
+
+type test struct {
+	foo int
+	bar string
+}
+
+type tester interface {
+	Test() bool
+}
+
+const five = 5
+
+func main() {
+	var a = 'a'
+	var b int = 0
+	myMap := map[string]int{
+		"a": 5,
+		"b": 7,
+	}
+	for key, count := range myMap {
+		fmt.Println(key, count)
+	}
+	defer fmt.Println(a)
+	for {
+		a ^= a
+		b++
+	}
+}
