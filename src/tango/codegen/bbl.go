882e10a1c4cbe7e750a97c88dc579ee166282fc6
Subhdeep Saha <subhdeepsaha@gmail.com>
2018-02-09 04:14:32 +0530

    Complete Register Allocation Algorithm

--- src/tango/codegen/bbl.go
+++ src/tango/codegen/bbl.go
@@ -3,7 +3,7 @@ package codegen
 // BBLEntry is Single entry in BBL List
 type BBLEntry struct {
 	Block []IRIns
-	Info  []map[SymbolTableEntry]UseInfo
+	Info  []map[*SymbolTableRegisterEntry]UseInfo
 }
 
 // UseInfo stores life and next Use Information of a variable
@@ -15,7 +15,7 @@ type UseInfo struct {
 // BBLList is the list of all the Basic Blocks
 var BBLList []BBLEntry
 
-var symbolInfo = make(map[SymbolTableEntry]UseInfo)
+var symbolInfo = make(map[*SymbolTableRegisterEntry]UseInfo)
 
 // GenBBLList takes the IRCode (list of IRIns) as input & creates list of basic blocks
 func GenBBLList(IRCode []IRIns) {
@@ -25,17 +25,17 @@ func GenBBLList(IRCode []IRIns) {
 	prevIndex := 0
 	for index, ins := range IRCode {
 		if arg1 := ins.Arg1; arg1 != nil {
-			if _, isRegister := arg1.(SymbolTableRegisterEntry); isRegister {
+			if arg1, isRegister := arg1.(*SymbolTableRegisterEntry); isRegister {
 				symbolInfo[arg1] = UseInfo{true, -1}
 			}
 		}
 		if arg2 := ins.Arg2; arg2 != nil {
-			if _, isRegister := arg2.(SymbolTableRegisterEntry); isRegister {
+			if arg2, isRegister := arg2.(*SymbolTableRegisterEntry); isRegister {
 				symbolInfo[arg2] = UseInfo{true, -1}
 			}
 		}
 		if dst := ins.Dst; dst != nil {
-			if _, isRegister := dst.(SymbolTableRegisterEntry); isRegister {
+			if dst, isRegister := dst.(*SymbolTableRegisterEntry); isRegister {
 				symbolInfo[dst] = UseInfo{true, -1}
 			}
 		}
@@ -57,28 +57,28 @@ func GenBBLList(IRCode []IRIns) {
 
 // Adds Operands' UseInfo in the BBL
 func addUseInfo(bbl BBLEntry) BBLEntry {
-	bbl.Info = make([]map[SymbolTableEntry]UseInfo, len(bbl.Block))
-	infomap := make(map[SymbolTableEntry]UseInfo)
+	bbl.Info = make([]map[*SymbolTableRegisterEntry]UseInfo, len(bbl.Block))
+	infomap := make(map[*SymbolTableRegisterEntry]UseInfo)
 	for i := len(bbl.Block) - 1; i >= 0; i-- {
 		if dst := bbl.Block[i].Dst; dst != nil {
-			if _, isRegister := dst.(SymbolTableRegisterEntry); isRegister {
+			if dst, isRegister := dst.(*SymbolTableRegisterEntry); isRegister {
 				infomap[dst] = symbolInfo[dst]
 				symbolInfo[dst] = UseInfo{false, -1}
 			}
 		}
 		if arg1 := bbl.Block[i].Arg1; arg1 != nil {
-			if _, isRegister := arg1.(SymbolTableRegisterEntry); isRegister {
+			if arg1, isRegister := arg1.(*SymbolTableRegisterEntry); isRegister {
 				infomap[arg1] = symbolInfo[arg1]
 				symbolInfo[arg1] = UseInfo{true, i}
 			}
 		}
 		if arg2 := bbl.Block[i].Arg2; arg2 != nil {
-			if _, isRegister := arg2.(SymbolTableRegisterEntry); isRegister {
+			if arg2, isRegister := arg2.(*SymbolTableRegisterEntry); isRegister {
 				infomap[arg2] = symbolInfo[arg2]
 				symbolInfo[arg2] = UseInfo{true, i}
 			}
 		}
-		ninfomap := make(map[SymbolTableEntry]UseInfo)
+		ninfomap := make(map[*SymbolTableRegisterEntry]UseInfo)
 		for k, v := range infomap {
 			ninfomap[k] = v
 		}
--- src/tango/codegen/reg.go
+++ src/tango/codegen/reg.go
@@ -1,6 +1,9 @@
 package codegen
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
 type address struct {
 	regLocation string
@@ -8,48 +11,93 @@ type address struct {
 }
 
 // Initialization of registers
-var registers = map[string]string{"r1": "", "r2": "", "r3": "", "r4": ""}
+var registers = map[string]*SymbolTableRegisterEntry{
+	"r1": nil,
+	"r2": nil,
+	"r3": nil,
+	"r4": nil,
+}
 
 //Initialization of variables
-var variables = make(map[string]address)
+var variables = make(map[*SymbolTableRegisterEntry]address)
 
-func assignRegister(i SymbolTableRegisterEntry) {
+func assignRegister(i *SymbolTableRegisterEntry, uinfo map[*SymbolTableRegisterEntry]UseInfo,
+	cannotbeReplaced map[string]bool, dst *SymbolTableRegisterEntry, canReplace bool) {
 
-	if val, ok := variables[i.Register]; ok && val.regLocation != "" {
+	if val, ok := variables[i]; ok && val.regLocation != "" {
+		cannotbeReplaced[val.regLocation] = true
 		return
 	}
 
 	for key, value := range registers {
-		if value == "" {
-			registers[key] = i.Register
-			variables[i.Register] = address{
-				regLocation: i.Register,
-				memLocation: i.Register,
-			}
+		if value == nil {
+			registers[key] = i
+			// Update current address of i
+			currentAddress := variables[i]
+			currentAddress.regLocation = key
+			variables[i] = currentAddress
+
+			// If value is true it is not used and maybe replaced by dst if required
+			cannotbeReplaced[key] = uinfo[i].NextUse == -1
 			return
 		}
 	}
 
+	score := make(map[string]int)
+
+	for key, value := range registers {
+		if val, ok := cannotbeReplaced[key]; ok && !(dst == i && val) {
+			score[key] = math.MaxInt32
+		}
+		if variables[value].memLocation == "" {
+			if !(canReplace && value == dst) {
+				if uinfo[value].NextUse != -1 {
+					score[key]++
+				}
+			}
+		}
+	}
+
+	minScore := math.MaxInt32
+	minScoreReg := ""
+
+	for register, s := range score {
+		if s < minScore {
+			minScore = s
+			minScoreReg = register
+		}
+	}
+
+	// TODO: Return the fact that spilling needs to be done for this register
+	// TODO: return the register being returned
+	registers[minScoreReg] = i
+	currentAddress := variables[i]
+	currentAddress.regLocation = minScoreReg
+	variables[i] = currentAddress
+
+	cannotbeReplaced[minScoreReg] = uinfo[i].NextUse == -1
+	return
 }
 
-func getReg(ins IRIns) {
+func getReg(ins IRIns, uinfo map[*SymbolTableRegisterEntry]UseInfo) {
 
 	instructionType := ins.Typ
 
+	cannotBeReplaced := make(map[string]bool)
+
 	switch instructionType {
 	case BOP:
-		if i, isRegister := ins.Arg1.(SymbolTableRegisterEntry); isRegister {
+		canReplace := ins.Dst != ins.Arg1 && ins.Dst != ins.Arg2
+		dst := ins.Dst.(*SymbolTableRegisterEntry)
+		if i, isRegister := ins.Arg1.(*SymbolTableRegisterEntry); isRegister {
 			//  i is a SymbolTableRegister
-			assignRegister(i)
+			assignRegister(i, uinfo, cannotBeReplaced, dst, canReplace)
 		}
-		if i, isRegister := ins.Arg2.(SymbolTableRegisterEntry); isRegister {
+		if i, isRegister := ins.Arg2.(*SymbolTableRegisterEntry); isRegister {
 			//  i is a SymbolTableRegister
-			assignRegister(i)
+			assignRegister(i, uinfo, cannotBeReplaced, dst, canReplace)
 		}
-		if i, isRegister := ins.Dst.(SymbolTableRegisterEntry); isRegister {
-			//  i is a SymbolTableRegister
-			assignRegister(i)
-		}
+		assignRegister(dst, uinfo, cannotBeReplaced, dst, canReplace)
 	case UOP:
 		fmt.Println("UOP")
 	case CBR:
