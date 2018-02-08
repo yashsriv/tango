187c13c62d26431959422c4e3222f6cd491167fe
Yash Srivastav <admin@yashsriv.org>
2018-02-08 23:56:21 +0530

    Read IR Code from file completed

--- src/tango/codegen/symb-table.go
+++ src/tango/codegen/symb-table.go
@@ -1,5 +1,10 @@
 package codegen
 
+import (
+	"log"
+	"strconv"
+)
+
 // SymbolTableEntry is an entry in the SymbolTable
 type SymbolTableEntry interface {
 }
@@ -18,4 +23,90 @@ type SymbolTableTargetEntry struct {
 // SymbolTableRegisterEntry refers to a register in the symbol table
 type SymbolTableRegisterEntry struct {
 	Register string
+}
+
+// SymbolTable is an array of SymbolTableEntries
+var SymbolTable []SymbolTableEntry
+var symbolMap = make(map[string]SymbolTableEntry)
+
+func insertToSymbolTable(val string) SymbolTableEntry {
+	var entry SymbolTableEntry
+	switch val[0] {
+	case '$':
+		i, err := strconv.ParseInt(val[1:], 0, 32)
+		if err != nil {
+			log.Fatalf("Unable to parse int: %d\nError: %v\n", i, err)
+		}
+
+		entry = &SymbolTableLiteralEntry{
+			Repr:  val,
+			Value: int(i),
+		}
+	case '#':
+		entry = &SymbolTableTargetEntry{
+			Target: val[1:],
+		}
+	case 'r':
+		entry = &SymbolTableRegisterEntry{
+			Register: val,
+		}
+	}
+	SymbolTable = append(SymbolTable, entry)
+	return entry
+}
+
+// GetRegs populates symbol table and gets virtual registers
+func GetRegs(splitted []string, typ IRType, op IROp) (SymbolTableEntry, SymbolTableEntry, SymbolTableEntry) {
+
+	var arg1, arg2, dst SymbolTableEntry
+	if typ == BOP || typ == CBR {
+		if val, ok := symbolMap[splitted[1]]; ok {
+			dst = val
+		} else {
+			dst = insertToSymbolTable(splitted[1])
+			symbolMap[splitted[1]] = dst
+		}
+		if val, ok := symbolMap[splitted[2]]; ok {
+			arg1 = val
+		} else {
+			arg1 = insertToSymbolTable(splitted[2])
+			symbolMap[splitted[1]] = arg1
+		}
+		if val, ok := symbolMap[splitted[3]]; ok {
+			arg2 = val
+		} else {
+			arg2 = insertToSymbolTable(splitted[3])
+			symbolMap[splitted[1]] = arg2
+		}
+	} else if typ == UOP || typ == ASN {
+		if val, ok := symbolMap[splitted[1]]; ok {
+			dst = val
+		} else {
+			dst = insertToSymbolTable(splitted[1])
+			symbolMap[splitted[1]] = dst
+		}
+		if val, ok := symbolMap[splitted[2]]; ok {
+			arg1 = val
+		} else {
+			arg1 = insertToSymbolTable(splitted[2])
+			symbolMap[splitted[1]] = arg1
+		}
+	} else if typ == JMP {
+		if val, ok := symbolMap[splitted[1]]; ok {
+			arg1 = val
+		} else {
+			arg1 = insertToSymbolTable(splitted[1])
+			symbolMap[splitted[1]] = arg1
+		}
+	} else if typ == KEY {
+		if !(op == RET || op == HALT) {
+			if val, ok := symbolMap[splitted[1]]; ok {
+				arg1 = val
+			} else {
+				arg1 = insertToSymbolTable(splitted[1])
+				symbolMap[splitted[1]] = arg1
+			}
+		}
+	}
+	return arg1, arg2, dst
 }
---
+++ src/tango/main/codegen/codegen.go
@@ -0,0 +1,71 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strings"
+
+	cg "tango/src/tango/codegen"
+)
+
+func main() {
+	if len(os.Args) < 2 {
+		fmt.Printf("Usage: %s <file name>\n", os.Args[0])
+		os.Exit(1)
+	}
+
+	file, err := os.Open(os.Args[1])
+	if err != nil {
+		log.Fatalf("Unable to read file: %s\nError: %v\n", os.Args[1], err)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+
+	var code []cg.IRIns
+
+	for scanner.Scan() {
+		line := scanner.Text()
+
+		// Get Label
+		label := cg.IRLabel("")
+		colonIndex := strings.Index(line, ":")
+		if colonIndex != -1 {
+			label = cg.IRLabel(line[:colonIndex])
+		}
+		line = line[colonIndex+1:]
+
+		// Split into various parts
+		splitted := strings.Split(line, ",")
+
+		// Get op code
+		op := cg.IROp(splitted[0])
+
+		// Get type
+		typ := cg.GetType(op)
+		if typ == cg.INV {
+			log.Fatal("Invalid operator: " + op)
+		}
+
+		// Get args
+		arg1, arg2, dst := cg.GetRegs(splitted, typ, op)
+
+		ins := cg.IRIns{
+			Typ:   typ,
+			Op:    op,
+			Arg1:  arg1,
+			Arg2:  arg2,
+			Dst:   dst,
+			Label: label,
+		}
+
+		code = append(code, ins)
+	}
+
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+
+}
