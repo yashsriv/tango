1bc6f728267a9f6d90e2a6e9c645d0c643995cf2
Yash Srivastav <admin@yashsriv.org>
2018-02-11 03:25:52 +0530

    Going to start binary operations
    We are now going to meditate for a few minutes and then
    begin working. Right now, loops work very well.
    
    Good work. #GoTango

--- src/codegen/codegen.go
+++ src/codegen/codegen.go
@@ -9,7 +9,7 @@ var Code string
 
 func spill(entries map[*SymbolTableVariableEntry]bool) {
 	for entry := range entries {
-		Code += fmt.Sprintf("movl %s, $%s\n", addrDesc[entry].regLocation, entry.MemoryLocation)
+		Code += fmt.Sprintf("movl %s, (%s)\n", addrDesc[entry].regLocation, entry.MemoryLocation)
 
 		delete(regDesc[addrDesc[entry].regLocation], entry)
 
@@ -72,128 +72,203 @@ func bopCode(ins IRIns, regs [3]MachineRegister) {
 	}
 }
 
-func op(ins IRIns, pointerMap map[*SymbolTableVariableEntry]*SymbolTableVariableEntry, regs [3]registerResult) {
+func genKeyCode(ins IRIns, regs [3]registerResult) {
+	switch ins.Op {
+	case CALL:
+		Code += fmt.Sprintf("call %s\n", ins.Arg1.(*SymbolTableTargetEntry).Target)
+	case PARAM:
+		if regs[0].Register == "" {
+			// This is a literal. Push directly.
+			Code += fmt.Sprintf("push %s\n", ins.Arg1.SymbolTableString())
+		} else {
+			// TODO: Instead of loading to register. If not in a register,
+			// then directly push from memory otherwise push from register
+			load(regs[0], ins.Arg1)
+			Code += fmt.Sprintf("push %s\n", regs[0].Register)
+		}
+	case RET:
+		Code += "ret\n"
+	case HALT:
+		Code += "call exit\n"
+	case PRINTINT:
+		if regs[0].Register == "" {
+			// This is a literal. Push directly.
+			Code += fmt.Sprintf("push %s\n", ins.Arg1.SymbolTableString())
+		} else {
+			load(regs[0], ins.Arg1)
+			Code += fmt.Sprintf("push %s\n", regs[0].Register)
+		}
+		Code += fmt.Sprintf("push $_fmtint\n")
+		Code += fmt.Sprintf("call printf\n")
+	case PRINTCHAR:
+		if regs[0].Register == "" {
+			// This is a literal. Push directly.
+			Code += fmt.Sprintf("push %s\n", ins.Arg1.SymbolTableString())
+		} else {
+			load(regs[0], ins.Arg1)
+			Code += fmt.Sprintf("push %s\n", regs[0].Register)
+		}
+		Code += fmt.Sprintf("push $_fmtchar\n")
+		Code += fmt.Sprintf("call printf\n")
+	case PRINTSTR:
+		if regs[0].Register == "" {
+			// This is a literal. Push directly.
+			Code += fmt.Sprintf("push %s\n", ins.Arg1.SymbolTableString())
+		} else {
+			load(regs[0], ins.Arg1)
+			Code += fmt.Sprintf("push %s\n", regs[0].Register)
+		}
+		Code += fmt.Sprintf("push $_fmtstr\n")
+		Code += fmt.Sprintf("call printf\n")
+	case SCANINT:
+
+		arg1 := ins.Arg1.(*SymbolTableVariableEntry)
+		Code += fmt.Sprintf("push $%s\n", arg1.MemoryLocation)
+		Code += fmt.Sprintf("push $_fmtint\n")
+		Code += fmt.Sprintf("call scanf\n")
+
+		// Invalidate registers if any
+		if addrDesc[arg1].regLocation != "" {
+			delete(regDesc[addrDesc[arg1].regLocation], arg1)
+			addrDesc[arg1] = address{
+				regLocation: "",
+				memLocation: arg1.MemoryLocation,
+			}
+		}
+
+	case SCANCHAR:
+		arg1 := ins.Arg1.(*SymbolTableVariableEntry)
+		Code += fmt.Sprintf("push $%s\n", arg1.MemoryLocation)
+		Code += fmt.Sprintf("push $_fmtchar\n")
+		Code += fmt.Sprintf("call scanf\n")
+		// Invalidate registers if any
+		if addrDesc[arg1].regLocation != "" {
+			delete(regDesc[addrDesc[arg1].regLocation], arg1)
+			addrDesc[arg1] = address{
+				regLocation: "",
+				memLocation: arg1.MemoryLocation,
+			}
+		}
+
+	case SCANSTR:
+		arg1 := ins.Arg1.(*SymbolTableVariableEntry)
+		Code += fmt.Sprintf("push $%s\n", arg1.MemoryLocation)
+		Code += fmt.Sprintf("push $_fmtstr\n")
+		Code += fmt.Sprintf("call scanf\n")
+		// Invalidate registers if any
+		if addrDesc[arg1].regLocation != "" {
+			delete(regDesc[addrDesc[arg1].regLocation], arg1)
+			addrDesc[arg1] = address{
+				regLocation: "",
+				memLocation: arg1.MemoryLocation,
+			}
+		}
+
+	case INC:
+		arg1 := ins.Arg1.(*SymbolTableVariableEntry)
+		if addrDesc[arg1].regLocation == "" {
+			Code += fmt.Sprintf("incl (%s)\n", arg1.MemoryLocation)
+		} else {
+			Code += fmt.Sprintf("incl %s\n", addrDesc[arg1].regLocation)
+			addrDesc[arg1] = address{
+				regLocation: addrDesc[arg1].regLocation,
+				memLocation: "",
+			}
+		}
+	case DEC:
+		arg1 := ins.Arg1.(*SymbolTableVariableEntry)
+		if addrDesc[arg1].regLocation == "" {
+			Code += fmt.Sprintf("decl (%s)\n", arg1.MemoryLocation)
+		} else {
+			Code += fmt.Sprintf("decl %s\n", addrDesc[arg1].regLocation)
+			addrDesc[arg1] = address{
+				regLocation: addrDesc[arg1].regLocation,
+				memLocation: "",
+			}
+		}
+	}
+}
+
+func genUOpCode(ins IRIns, regs [3]registerResult) {
+	switch ins.Op {
+	case NEG:
+		var valueString string
+		if regs[0].Register == "" {
+			valueString = ins.Arg1.SymbolTableString()
+		} else {
+			load(regs[0], ins.Arg1)
+			valueString = string(regs[0].Register)
+		}
+		spill(regs[2].Spill)
+		Code += fmt.Sprintf("movl %s, %s\n", valueString, regs[2].Register)
+		Code += fmt.Sprintf("negl %s\n", regs[2].Register)
+		regDesc[regs[2].Register][ins.Dst.(*SymbolTableVariableEntry)] = true
+		addrDesc[ins.Dst.(*SymbolTableVariableEntry)] = address{
+			regLocation: regs[2].Register,
+			memLocation: "",
+		}
+	case BNOT:
+		load(regs[0], ins.Arg1)
+		spill(regs[2].Spill)
+		Code += fmt.Sprintf("movl %s, %s\n", regs[0].Register, regs[2].Register)
+		Code += fmt.Sprintf("notl %s\n", regs[2].Register)
+		regDesc[regs[2].Register][ins.Dst.(*SymbolTableVariableEntry)] = true
+		addrDesc[ins.Dst.(*SymbolTableVariableEntry)] = address{
+			regLocation: regs[2].Register,
+			memLocation: "",
+		}
+	case VAL:
+		log.Fatalf("Unhandled pointer stuff")
+		// TODO: Discuss with Sir
+		// Maintain a pointer map while dereferencing. Check if what we
+		// want to dereference is in registers
+	case ADDR:
+		log.Fatalf("Unhandled pointer stuff")
+		// TODO: Discuss with Sir
+		// Maintain a pointer map while dereferencing. Check if what we
+		// want to dereference is in registers
+	}
+}
+
+func genCBRCode(ins IRIns, regs [3]registerResult) {
+	var op1, op2 string
+	if regs[0].Register == "" {
+		op1 = ins.Arg1.SymbolTableString()
+	} else {
+		load(regs[0], ins.Arg1)
+		op1 = string(regs[0].Register)
+	}
+	if regs[1].Register == "" {
+		log.Fatalf("2nd Operand of cmp cannot be a constant")
+	} else {
+		load(regs[1], ins.Arg2)
+		op2 = string(regs[1].Register)
+	}
+	Code += fmt.Sprintf("cmpl %s, %s\n", op1, op2)
+	switch ins.Op {
+	case BREQ:
+		Code += fmt.Sprintf("je %s\n", ins.Dst.(*SymbolTableTargetEntry).Target)
+	case BRNEQ:
+		Code += fmt.Sprintf("jne %s\n", ins.Dst.(*SymbolTableTargetEntry).Target)
+	case BRLT:
+		Code += fmt.Sprintf("jl %s\n", ins.Dst.(*SymbolTableTargetEntry).Target)
+	case BRLTE:
+		Code += fmt.Sprintf("jle %s\n", ins.Dst.(*SymbolTableTargetEntry).Target)
+	case BRGT:
+		Code += fmt.Sprintf("jg %s\n", ins.Dst.(*SymbolTableTargetEntry).Target)
+	case BRGTE:
+		Code += fmt.Sprintf("jge %s\n", ins.Dst.(*SymbolTableTargetEntry).Target)
+	}
+}
+
+func genOpCode(ins IRIns, pointerMap map[*SymbolTableVariableEntry]*SymbolTableVariableEntry, regs [3]registerResult) {
 	switch ins.Typ {
 	case LBL:
 		Code += fmt.Sprintf("%s:\n", ins.Dst.(*SymbolTableTargetEntry).Target)
 	case JMP:
 		Code += fmt.Sprintf("jmp %s\n", ins.Arg1.(*SymbolTableTargetEntry).Target)
 	case KEY:
-		switch ins.Op {
-		case CALL:
-			Code += fmt.Sprintf("call %s\n", ins.Arg1.(*SymbolTableTargetEntry).Target)
-		case PARAM:
-			if regs[0].Register == "" {
-				// This is a literal. Push directly.
-				Code += fmt.Sprintf("push %s\n", ins.Arg1.SymbolTableString())
-			} else {
-				// TODO: Instead of loading to register. If not in a register,
-				// then directly push from memory otherwise push from register
-				load(regs[0], ins.Arg1)
-				Code += fmt.Sprintf("push %s\n", regs[0].Register)
-			}
-		case RET:
-			Code += "ret\n"
-		case HALT:
-			Code += "call exit\n"
-			// TODO: Add _fmt* to .data segment
-		case PRINTINT:
-			if regs[0].Register == "" {
-				// This is a literal. Push directly.
-				Code += fmt.Sprintf("push %s\n", ins.Arg1.SymbolTableString())
-			} else {
-				load(regs[0], ins.Arg1)
-				Code += fmt.Sprintf("push %s\n", regs[0].Register)
-			}
-			Code += fmt.Sprintf("push $_fmtint\n")
-			Code += fmt.Sprintf("call printf\n")
-		case PRINTCHAR:
-			if regs[0].Register == "" {
-				// This is a literal. Push directly.
-				Code += fmt.Sprintf("push %s\n", ins.Arg1.SymbolTableString())
-			} else {
-				load(regs[0], ins.Arg1)
-				Code += fmt.Sprintf("push %s\n", regs[0].Register)
-			}
-			Code += fmt.Sprintf("push $_fmtchar\n")
-			Code += fmt.Sprintf("call printf\n")
-		case PRINTSTR:
-			if regs[0].Register == "" {
-				// This is a literal. Push directly.
-				Code += fmt.Sprintf("push %s\n", ins.Arg1.SymbolTableString())
-			} else {
-				load(regs[0], ins.Arg1)
-				Code += fmt.Sprintf("push %s\n", regs[0].Register)
-			}
-			Code += fmt.Sprintf("push $_fmtstr\n")
-			Code += fmt.Sprintf("call printf\n")
-		case SCANINT:
-
-			arg1 := ins.Arg1.(*SymbolTableVariableEntry)
-			Code += fmt.Sprintf("push $%s\n", arg1.MemoryLocation)
-			Code += fmt.Sprintf("push $_fmtint\n")
-			Code += fmt.Sprintf("call scanf\n")
-
-			// Invalidate registers if any
-			if addrDesc[arg1].regLocation != "" {
-				delete(regDesc[addrDesc[arg1].regLocation], arg1)
-				addrDesc[arg1] = address{
-					regLocation: "",
-					memLocation: arg1.MemoryLocation,
-				}
-			}
-
-		case SCANCHAR:
-			arg1 := ins.Arg1.(*SymbolTableVariableEntry)
-			Code += fmt.Sprintf("push $%s\n", arg1.MemoryLocation)
-			Code += fmt.Sprintf("push $_fmtchar\n")
-			Code += fmt.Sprintf("call scanf\n")
-			// Invalidate registers if any
-			if addrDesc[arg1].regLocation != "" {
-				delete(regDesc[addrDesc[arg1].regLocation], arg1)
-				addrDesc[arg1] = address{
-					regLocation: "",
-					memLocation: arg1.MemoryLocation,
-				}
-			}
-
-		case SCANSTR:
-			arg1 := ins.Arg1.(*SymbolTableVariableEntry)
-			Code += fmt.Sprintf("push $%s\n", arg1.MemoryLocation)
-			Code += fmt.Sprintf("push $_fmtstr\n")
-			Code += fmt.Sprintf("call scanf\n")
-			// Invalidate registers if any
-			if addrDesc[arg1].regLocation != "" {
-				delete(regDesc[addrDesc[arg1].regLocation], arg1)
-				addrDesc[arg1] = address{
-					regLocation: "",
-					memLocation: arg1.MemoryLocation,
-				}
-			}
-
-		case INC:
-			arg1 := ins.Arg1.(*SymbolTableVariableEntry)
-			if addrDesc[arg1].regLocation == "" {
-				Code += fmt.Sprintf("inc $%s\n", arg1.MemoryLocation)
-			} else {
-				Code += fmt.Sprintf("inc %s\n", addrDesc[arg1].regLocation)
-				addrDesc[arg1] = address{
-					regLocation: addrDesc[arg1].regLocation,
-					memLocation: "",
-				}
-			}
-		case DEC:
-			arg1 := ins.Arg1.(*SymbolTableVariableEntry)
-			if addrDesc[arg1].regLocation == "" {
-				Code += fmt.Sprintf("dec $%s\n", arg1.MemoryLocation)
-			} else {
-				Code += fmt.Sprintf("dec %s\n", addrDesc[arg1].regLocation)
-				addrDesc[arg1] = address{
-					regLocation: addrDesc[arg1].regLocation,
-					memLocation: "",
-				}
-			}
-		}
+		genKeyCode(ins, regs)
 	case ASN:
 		load(regs[0], ins.Arg1)
 		regDesc[regs[0].Register][ins.Dst.(*SymbolTableVariableEntry)] = true
@@ -202,86 +277,9 @@ func op(ins IRIns, pointerMap map[*SymbolTableVariableEntry]*SymbolTableVariable
 			memLocation: "",
 		}
 	case UOP:
-		switch ins.Op {
-		case NEG:
-			load(regs[0], ins.Arg1)
-			spill(regs[2].Spill)
-			Code += fmt.Sprintf("movl %s, %s\n", regs[0].Register, regs[2].Register)
-			Code += fmt.Sprintf("neg %s\n", regs[2].Register)
-			regDesc[regs[2].Register][ins.Dst.(*SymbolTableVariableEntry)] = true
-			addrDesc[ins.Dst.(*SymbolTableVariableEntry)] = address{
-				regLocation: regs[2].Register,
-				memLocation: "",
-			}
-		case BNOT:
-			load(regs[0], ins.Arg1)
-			spill(regs[2].Spill)
-			Code += fmt.Sprintf("movl %s, %s\n", regs[0].Register, regs[2].Register)
-			Code += fmt.Sprintf("not %s\n", regs[2].Register)
-			regDesc[regs[2].Register][ins.Dst.(*SymbolTableVariableEntry)] = true
-			addrDesc[ins.Dst.(*SymbolTableVariableEntry)] = address{
-				regLocation: regs[2].Register,
-				memLocation: "",
-			}
-		case VAL:
-			log.Fatalf("Unhandled pointer stuff")
-			// TODO: Discuss with Sir
-			// Maintain a pointer map while dereferencing. Check if what we
-			// want to dereference is in registers
-			variable, ok := pointerMap[ins.Arg1.(*SymbolTableVariableEntry)]
-			if !ok {
-				log.Fatalf("Pointer being dereferenced: %s is not existing in our map.", ins.Arg1.SymbolTableString())
-			}
-
-			spill(regs[2].Spill)
-			// The dereferenced value is in a register. Move from the register.
-			if addrDesc[variable].regLocation != "" {
-				Code += fmt.Sprintf("movl %s, %s\n", addrDesc[variable].regLocation, regs[2].Register)
-			} else {
-				Code += fmt.Sprintf("movl (%s), %s\n", addrDesc[variable].memLocation, regs[2].Register)
-			}
-
-			regDesc[regs[2].Register][ins.Dst.(*SymbolTableVariableEntry)] = true
-			addrDesc[ins.Dst.(*SymbolTableVariableEntry)] = address{
-				regLocation: regs[2].Register,
-				memLocation: "",
-			}
-		case ADDR:
-			log.Fatalf("Unhandled pointer stuff")
-			// TODO: Discuss with Sir
-			// Maintain a pointer map while dereferencing. Check if what we
-			// want to dereference is in registers
-			pointerMap[ins.Dst.(*SymbolTableVariableEntry)] = ins.Arg1.(*SymbolTableVariableEntry)
-		}
+		genUOpCode(ins, regs)
 	case CBR:
-		var op1, op2 string
-		if regs[0].Register == "" {
-			op1 = ins.Arg1.SymbolTableString()
-		} else {
-			load(regs[0], ins.Arg1)
-			op1 = string(regs[0].Register)
-		}
-		if regs[1].Register == "" {
-			op2 = ins.Arg2.SymbolTableString()
-		} else {
-			load(regs[1], ins.Arg2)
-			op2 = string(regs[1].Register)
-		}
-		Code += fmt.Sprintf("cmp %s, %s\n", op1, op2)
-		switch ins.Op {
-		case BREQ:
-			Code += fmt.Sprintf("je %s\n", ins.Dst.(*SymbolTableTargetEntry).Target)
-		case BRNEQ:
-			Code += fmt.Sprintf("jne %s\n", ins.Dst.(*SymbolTableTargetEntry).Target)
-		case BRLT:
-			Code += fmt.Sprintf("jl %s\n", ins.Dst.(*SymbolTableTargetEntry).Target)
-		case BRLTE:
-			Code += fmt.Sprintf("jle %s\n", ins.Dst.(*SymbolTableTargetEntry).Target)
-		case BRGT:
-			Code += fmt.Sprintf("jg %s\n", ins.Dst.(*SymbolTableTargetEntry).Target)
-		case BRGTE:
-			Code += fmt.Sprintf("jge %s\n", ins.Dst.(*SymbolTableTargetEntry).Target)
-		}
+		genCBRCode(ins, regs)
 	default:
 		log.Fatalf("Unhandled instruction: %s\n", ins.String())
 	}
@@ -291,18 +289,30 @@ func genCode() {
 	pointerMap := make(map[*SymbolTableVariableEntry]*SymbolTableVariableEntry)
 
 	for _, bbl := range BBLList {
+		Code += "# Begin Basic Block\n"
 		for i, ins := range bbl.Block {
 			arg1res, arg2res, dstres := getReg(ins, bbl.Info[i])
 
-			op(ins, pointerMap, [3]registerResult{arg1res, arg2res, dstres})
+			if i == len(bbl.Block)-1 && shouldPreSave(ins) {
+				saveBBL()
+			}
+
+			genOpCode(ins, pointerMap, [3]registerResult{arg1res, arg2res, dstres})
+
+			if i == len(bbl.Block)-1 && !shouldPreSave(ins) {
+				saveBBL()
+			}
 
 		}
-		saveBBL()
+		for register := range regDesc {
+			regDesc[register] = make(map[*SymbolTableVariableEntry]bool)
+		}
+		Code += "# End Basic Block\n"
 	}
 }
 
 func genData() {
-	Code += ".section .data\n"
+	Code += ".section .data\n\n"
 	Code += "_fmtint: .string \"%d\"\n"
 	Code += "_fmtchar: .string \"%c\"\n"
 	Code += "_fmtstr: .string \"%s\"\n"
@@ -314,22 +324,23 @@ func genData() {
 }
 
 func saveBBL() {
+	// Code += "\n\n# Saving Stuff\n"
 	for register, variables := range regDesc {
 		for variable := range variables {
 			if addrDesc[variable].memLocation == "" {
 				Code += fmt.Sprintf("movl %s, (%s)\n", register, variable.MemoryLocation)
-				addrDesc[variable] = address{
-					regLocation: register,
-					memLocation: variable.MemoryLocation,
-				}
+			}
+			addrDesc[variable] = address{
+				regLocation: "",
+				memLocation: variable.MemoryLocation,
 			}
 		}
 	}
+	// Code += "# Done Saving Stuff\n\n"
 }
 
 func genMisc() {
-	Code += `
-.section .text
+	Code += `.section .text
 .globl main
 
 `
@@ -340,4 +351,9 @@ func GenerateASM() {
 	genMisc()
 	genCode()
 	genData()
+}
+
+func shouldPreSave(ins IRIns) bool {
+	return ins.Typ == CBR || ins.Typ == JMP ||
+		(ins.Typ == KEY && (ins.Op != PARAM && ins.Op != INC && ins.Op != DEC))
 }
