5c56c80c425b5509e6ae2cdaa02973136ea950ff
Yash Srivastav <admin@yashsriv.org>
2018-04-20 09:48:02 +0530

    Add compiler options

--- src/main/tango/tango.go
+++ src/main/tango/tango.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"io"
 	"os"
 	"os/exec"
 
@@ -12,43 +14,80 @@ import (
 )
 
 func main() {
-	if len(os.Args) < 2 {
-		fmt.Printf("Usage: %s <file name>\n", os.Args[0])
+
+	langPtr := flag.String("x", "None", "Language to output. Possible values are IR, ASM and None")
+	outFile := flag.String("o", "./a.out", "Filename of output executable. Only valid with language None")
+
+	flag.Parse()
+
+	if len(flag.Args()) != 1 {
+		fmt.Printf("Usage: %s [<options>] <file name>\n", os.Args[0])
 		os.Exit(1)
 	}
-	lex, err := lexer.NewWrapperFile(os.Args[1])
+	lex, err := lexer.NewWrapperFile(flag.Args()[0])
 	if err != nil {
-		fmt.Printf("Unable to read file: %s\n", os.Args[1])
+		fmt.Printf("Unable to read file: %s\n", flag.Args()[0])
 		return
 	}
 	p := parser.NewParser()
 	st, err := p.Parse(lex)
 	if err != nil {
-		panic(err)
+		fmt.Fprintf(os.Stderr, "Error: An error occurred while parsing: %v\n", err)
+		os.Exit(1)
 	}
 	sourceFile, ok := st.(*ast.AddrCode)
-	if !ok {
-		panic("Expected an AddrCode")
+	if !ok || !sourceFile.TopLevel {
+		fmt.Fprintln(os.Stderr, "Error: Unexpected end of file")
+		os.Exit(1)
 	}
 
-	// for _, val := range sourceFile.Code {
-	// 	fmt.Fprintln(os.Stderr, val)
-	// }
-	codegen.GenBBLList(sourceFile.Code)
-	codegen.GenerateASM()
-
-	fmt.Println(codegen.Code)
-
-	gccCmd := exec.Command("gcc", "-m32", "-o", "prog", "-x", "assembler", "-")
-
-	gccIn, err := gccCmd.StdinPipe()
-	if err != nil {
-		panic(err)
+	if *langPtr != "None" && *outFile != "./a.out" {
+		fmt.Fprintln(os.Stderr, "Warning: -o flag is only valid when -x is None (the default)")
 	}
 
-	gccCmd.Start()
-	gccIn.Write([]byte(codegen.Code))
-	gccIn.Close()
-	gccCmd.Wait()
+	switch *langPtr {
+	case "IR":
+		for _, val := range sourceFile.Code {
+			fmt.Println(val)
+		}
+	case "ASM", "None":
+		codegen.GenBBLList(sourceFile.Code)
+		codegen.GenerateASM()
+		if *langPtr == "ASM" {
+			fmt.Println(codegen.Code)
+			break
+		}
+		gccCmd := exec.Command("gcc", "-m32", "-o", *outFile, "-x", "assembler", "-")
+		gccIn, err := gccCmd.StdinPipe()
+		if err != nil {
+			panic(err)
+		}
+		gccOut, err := gccCmd.StdoutPipe()
+		if err != nil {
+			panic(err)
+		}
+		gccErr, err := gccCmd.StderrPipe()
+		if err != nil {
+			panic(err)
+		}
+		gccCmd.Start()
+		gccIn.Write([]byte(codegen.Code))
+		gccIn.Close()
+
+		_, err = io.Copy(os.Stdout, gccOut)
+		if err != nil {
+			panic(err)
+		}
+		_, err = io.Copy(os.Stderr, gccErr)
+		if err != nil {
+			panic(err)
+		}
+		gccCmd.Wait()
+
+	default:
+		fmt.Fprintf(os.Stderr, "Error: Unsupported -x param: %s\n", *langPtr)
+		os.Exit(1)
+
+	}
 
 }
